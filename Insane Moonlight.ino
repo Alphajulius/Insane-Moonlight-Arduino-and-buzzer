e298834a3fac228183188d4dcaf2b82a2e73b8cd
TECHandMAKE <103307740+TECHandMAKE@users.noreply.github.com>
2023-09-19 17:51:21 +0200

    Add files via upload

---
+++ Insane Moonlight.ino
@@ -0,0 +1,44 @@
+int buzzer = 2;
+
+void setup() {
+  
+  pinMode(buzzer, OUTPUT);
+
+}
+
+void loop() {
+  tone(2,261.6,200);
+  delay(500);
+  noTone(2);
+  tone(2,261.6,200);
+  delay(500);
+  noTone(2);
+  tone(2,261.6,200);
+  delay(500);
+  noTone(2);
+  tone(2,293.7,100);
+  delay(500);
+  noTone(2);
+   tone(2,329.6,200);
+  delay(500);
+  noTone(2);
+   tone(2,293.7,100);
+  delay(500);
+  noTone(2);
+  tone(2,261.6,200);
+  delay(500);
+  noTone(2);
+  tone(2,329.6,200);
+  delay(500);
+  noTone(2);
+  tone(2,293.7,100);
+  delay(500);
+  noTone(2);
+   tone(2,293.7,100);
+  delay(500);
+  noTone(2);
+  tone(2,261.6,200);
+  delay(500);
+  noTone(2);
+
+}
